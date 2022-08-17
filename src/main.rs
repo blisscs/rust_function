68f3c69a54549a864256edd269f570108a7630ac
Suracheth Chawla <dev1987@gmail.com>
2022-08-17 22:00:42 +0700

    use `{` and `}` to create a new scope and return an expression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // {.. different scope..};
+    let y = {
+        let x = 3; // statement requires a semi-colon
+        x + 1 
+        // expression doesn't require semi-colon
+        // this is what is returned from this scope
+    };
 
-    print_labeled_measurement(5, 'h');
-}
-
-fn print_labeled_measurement(value: u8, unit_label: char) {
-    println!("The measurement is {}{}", value, unit_label);
+    println!("The value y is {}", y);
 }
