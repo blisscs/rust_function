15e3bffa0834d4bc1f0576d332472a29e9572757
Suracheth Chawla <dev1987@gmail.com>
2022-08-17 21:53:35 +0700

    function can take multiple parameters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(1);
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function(x: u8) {
-    println!("The value of x is {}", x);
+fn print_labeled_measurement(value: u8, unit_label: char) {
+    println!("The measurement is {}{}", value, unit_label);
 }
